580efc5f87b14b6c3314daa00edf0d5bba5b6fec
John Ace Magno <127085144+jamydevelop@users.noreply.github.com>
2024-08-21 13:58:26 +0800

    modified card container

--- wilcon_phase2/lib/module/homepage/widget/text_widget/recent_checklist_text_widget.dart
+++ wilcon_phase2/lib/module/homepage/widget/text_widget/recent_checklist_text_widget.dart
@@ -11,17 +11,22 @@ class RecentChecklistTextWidget extends StatefulWidget {
 class _RecentChecklistTextWidgetState extends State<RecentChecklistTextWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'Recent Checklist',
-      style: TextStyle(
-        color: Color(0xFF1B1D21), // Palette-Grey-900
-        fontFamily: 'Open Sans',
-        fontSize: 16,
-        fontStyle: FontStyle.normal,
-        fontWeight: FontWeight.w700, // Bold (700)
-        height:
-            1.25, // line-height of 20px for a font-size of 16px (20/16 = 1.25)
-        letterSpacing: 0.024,
+    return Container(
+      padding: const EdgeInsets.symmetric(
+          horizontal: 8.0), // 8px padding on left and right
+      alignment: Alignment.topLeft, // Align to the center left of the screen
+      child: const Text(
+        'Recent Checklist',
+        style: TextStyle(
+          color: Color(0xFF1B1D21), // Palette-Grey-900
+          fontFamily: 'Open Sans',
+          fontSize: 16,
+          fontStyle: FontStyle.normal,
+          fontWeight: FontWeight.w700, // Bold (700)
+          height:
+              1.25, // line-height of 20px for a font-size of 16px (20/16 = 1.25)
+          letterSpacing: 0.024,
+        ),
       ),
     );
   }
