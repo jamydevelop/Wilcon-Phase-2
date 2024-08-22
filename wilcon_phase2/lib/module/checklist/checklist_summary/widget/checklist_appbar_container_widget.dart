c29b3b1cb165ffa449d8d8e4d9b4427244d1a437
John Ace Magno <127085144+jamydevelop@users.noreply.github.com>
2024-08-22 15:09:45 +0800

    checklist summary page

---
+++ wilcon_phase2/lib/module/checklist/checklist_summary/widget/checklist_appbar_container_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:wilcon_phase2/widget/customized_appbar/appbar_widget.dart';
+import 'package:wilcon_phase2/widget/customized_appbar/back_screen_widget.dart';
+import 'package:wilcon_phase2/widget/header_bar_widget.dart';
+
+class ChecklistAppbarContainerWidget extends StatefulWidget {
+  const ChecklistAppbarContainerWidget({super.key});
+
+  @override
+  State<ChecklistAppbarContainerWidget> createState() =>
+      _ChecklistAppbarContainerWidgetState();
+}
+
+class _ChecklistAppbarContainerWidgetState
+    extends State<ChecklistAppbarContainerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 12),
+        child: Column(
+          children: [
+            SizedBox(height: 24),
+            CustomizedAppBar(),
+            BackToPrevScreen(),
+            SizedBox(height: 12),
+            HeaderBar(
+              text: 'Checklist #432',
+              isYellow: false,
+            ),
+            SizedBox(height: 24),
+          ],
+        ),
+      ),
+    );
+  }
+}
