859431f682cb4b55aaf63bdcc8a8b56c3fd1d8a2
John Ace Magno <127085144+jamydevelop@users.noreply.github.com>
2024-08-23 12:44:17 +0800

    update draflist page

--- wilcon_phase2/lib/module/draftlist/presentation/draft_list_page.dart
+++ wilcon_phase2/lib/module/draftlist/presentation/draft_list_page.dart
@@ -18,15 +18,12 @@ class _DraftlistPageState extends State<DraftlistPage> {
         backgroundColor: Colors.white,
         endDrawer: CustomSidebar(),
         body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 12.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                DraftListAppbarContainerWidget(),
-                DraftlistPageBodyWidget(),
-              ],
-            ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              DraftListAppbarContainerWidget(),
+              DraftlistPageBodyWidget(),
+            ],
           ),
         ),
       ),
