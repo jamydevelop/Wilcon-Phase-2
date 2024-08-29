6708a632d8bda86e6753e67bafc2226e491bc9bf
John Ace Magno <127085144+jamydevelop@users.noreply.github.com>
2024-08-29 21:14:23 +0800

    Project List Card updated

--- wilcon_phase2/lib/module/project/project_list/widget/project_list_content_body_container_widget.dart
+++ wilcon_phase2/lib/module/project/project_list/widget/project_list_content_body_container_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:wilcon_phase2/module/homepage/widget/homepage_card_widget.dart';
+import 'package:wilcon_phase2/module/project/project_details/presentation/project_details_page.dart';
+import 'package:wilcon_phase2/module/project/project_list/widget/project_list_card_widget.dart';
 
 class ProjectListContentBodyContainerWidget extends StatefulWidget {
   const ProjectListContentBodyContainerWidget({super.key});
@@ -13,32 +14,58 @@ class _ProjectListContentBodyContainerWidgetState
     extends State<ProjectListContentBodyContainerWidget> {
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
+    return SizedBox(
       width: double.infinity,
       // color: const Color(0xFFF4F7FA),
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 12),
+        padding: const EdgeInsets.symmetric(horizontal: 12),
         child: Column(
           children: [
-            HomepageCardWidget(
+            ProjectListCardWidget(
               mainTitle: 'Project ID#11',
               subTitle: 'completed',
-              rowTitles: ['Name', 'Location', 'status'],
-              rowContents: ['D68-Naic Renovation', 'D68-Naic', 'Active'],
+              rowTitles: const ['Name', 'Location', 'status'],
+              rowContents: const ['D68-Naic Renovation', 'D68-Naic', 'Active'],
+              onPressed: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ProjectDetailsPage()));
+              },
             ),
-            SizedBox(height: 12),
-            HomepageCardWidget(
+            const SizedBox(height: 12),
+            ProjectListCardWidget(
               mainTitle: 'Project ID#11',
               subTitle: 'completed',
-              rowTitles: ['Name', 'Location', 'status'],
-              rowContents: ['D68-Naic Renovation', 'D68-Naic', 'Inactive'],
+              rowTitles: const ['Name', 'Location', 'status'],
+              rowContents: const [
+                'D68-Naic Renovation',
+                'D68-Naic',
+                'Inactive'
+              ],
+              onPressed: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ProjectDetailsPage()));
+              },
             ),
-            SizedBox(height: 12),
-            HomepageCardWidget(
+            const SizedBox(height: 12),
+            ProjectListCardWidget(
               mainTitle: 'Project ID#11',
               subTitle: 'completed',
-              rowTitles: ['Name', 'Location', 'status'],
-              rowContents: ['D68-Naic Renovation', 'D68-Naic', 'Inactive'],
+              rowTitles: const ['Name', 'Location', 'status'],
+              rowContents: const [
+                'D68-Naic Renovation',
+                'D68-Naic',
+                'Inactive'
+              ],
+              onPressed: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ProjectDetailsPage()));
+              },
             ),
 
             // Additional content can be added here
